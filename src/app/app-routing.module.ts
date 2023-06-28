71d76d7b93c6a117d360f74a50c4704a13c79461
leiboT <superviho1@gmail.com>
2023-06-28 19:50:17 +0800

    ci: 环境变量

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { environment } from '@/environments/environment'
 
 const routes: Routes = []
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: environment.NOT_SUPPORT_HISTORY
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  SYSTEM_NAME: 'l-admin',
+  VK: 'kMglZ9B0ZLBYs9vkIQmGrBeB3Hpjpuwr',
+  NOT_SUPPORT_HISTORY: false,
+  SERVER_IS_MOCK: true
+}
