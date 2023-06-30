98f8194b599b382d7744b58a43ac7ef8e755d428
leiboT <superviho1@gmail.com>
2023-07-01 01:05:22 +0800

    chore: 目录重构

--- src/app/product-management/product-add-or-edit/product-add-or-edit.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-product-add-or-edit',
-  templateUrl: './product-add-or-edit.component.html',
-  styleUrls: ['./product-add-or-edit.component.less']
-})
-export class ProductAddOrEditComponent {}
--- src/app/product-management/product-list/product-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.less']
-})
-export class ProductListComponent {}
