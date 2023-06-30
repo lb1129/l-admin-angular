fd5f50bbc77e078dbb05761c04de06e194804dce
leiboT <superviho1@gmail.com>
2023-07-01 01:04:24 +0800

    chore: 目录重构

--- src/app/authenticate/find-password/find-password.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-find-password',
-  templateUrl: './find-password.component.html',
-  styleUrls: ['./find-password.component.less']
-})
-export class FindPasswordComponent {}
