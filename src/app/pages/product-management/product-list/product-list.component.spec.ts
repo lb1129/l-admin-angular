ba489b8720e3e74ae0d57d3c2c1ec3ed203e8689
leiboT <superviho1@gmail.com>
2023-07-01 21:29:31 +0800

    refactor: standalone模式

--- src/app/pages/product-management/product-list/product-list.component.spec.ts
+++ src/app/pages/product-management/product-list/product-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideRouter } from '@angular/router'
+import { provideAnimations } from '@angular/platform-browser/animations'
 
-import { ProductListComponent } from './product-list.component'
+import ProductListComponent from './product-list.component'
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent
@@ -8,7 +10,7 @@ describe('ProductListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductListComponent]
+      providers: [provideRouter([]), provideAnimations()]
     })
     fixture = TestBed.createComponent(ProductListComponent)
     component = fixture.componentInstance
