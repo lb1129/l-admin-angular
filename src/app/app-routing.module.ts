39f13d9eeb4054f263a5b6bea1fc50a6eed1ea4c
leiboT <superviho1@gmail.com>
2023-07-01 21:32:09 +0800

    refactor: standalone模式

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,60 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { environment } from '@/environments/environment'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/index/index/index.component'),
+    children: [
+      { path: '', loadComponent: () => import('./pages/index/home/home.component') },
+      {
+        path: 'productManagement',
+        children: [
+          { path: '', redirectTo: 'productList', pathMatch: 'full' },
+          {
+            path: 'productList',
+            loadComponent: () =>
+              import('./pages/product-management/product-list/product-list.component')
+          },
+          {
+            path: 'productDetail/:id',
+            loadComponent: () =>
+              import('./pages/product-management/product-detail/product-detail.component')
+          },
+          {
+            path: 'productAddOrEdit/:id?',
+            loadComponent: () =>
+              import('./pages/product-management/product-add-or-edit/product-add-or-edit.component')
+          }
+        ]
+      },
+      {
+        path: 'personalCenter',
+        loadComponent: () => import('./pages/personal-center/index/index.component')
+      }
+    ]
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/authenticate/login/login.component')
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/authenticate/register/register.component')
+  },
+  {
+    path: 'findPassword',
+    loadComponent: () => import('./pages/authenticate/find-password/find-password.component')
+  },
+  {
+    path: 'privacyPolicy',
+    loadComponent: () => import('./pages/sundry/privacy-policy/privacy-policy.component')
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/sundry/not-found/not-found.component')
+  }
+]
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { registerLocaleData } from '@angular/common'
 import zh from '@angular/common/locales/zh'
+
 import { NZ_I18N } from 'ng-zorro-antd/i18n'
 import { zh_CN } from 'ng-zorro-antd/i18n'
 
-import { AuthenticateModule } from './pages/authenticate/authenticate.module'
-import { IndexModule } from './pages/index/index.module'
-import { SundryModule } from './pages/sundry/sundry.module'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
 registerLocaleData(zh)
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthenticateModule, IndexModule, SundryModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
