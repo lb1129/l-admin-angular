4ea72a09803bdfe06ee8510f0fa58f894a3ddfdf
leiboT <superviho1@gmail.com>
2023-07-03 00:37:52 +0800

    chore: http拦截器注入,NzMessageService注入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { registerLocaleData } from '@angular/common'
@@ -8,16 +8,23 @@ import zh from '@angular/common/locales/zh'
 
 import { NZ_I18N } from 'ng-zorro-antd/i18n'
 import { zh_CN } from 'ng-zorro-antd/i18n'
+import { NzMessageService } from 'ng-zorro-antd/message'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+
+import { Interceptor } from '@/app/http/interceptor'
 
 registerLocaleData(zh)
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [
+    NzMessageService,
+    { provide: NZ_I18N, useValue: zh_CN },
+    { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
