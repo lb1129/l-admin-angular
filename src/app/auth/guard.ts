b9b2fbadf54dac7a186459bd8b1cdd9bff27a2cd
leiboT <superviho1@gmail.com>
2023-07-03 22:48:59 +0800

    chore: 更新 routeSnapshot store 以及 replace url

--- src/app/auth/guard.ts
+++ src/app/auth/guard.ts
@@ -2,20 +2,24 @@ import { inject } from '@angular/core'
 import { ActivatedRouteSnapshot, Router } from '@angular/router'
 import isAuthenticated from '@/app/auth/isAuthenticated'
 import { map } from 'rxjs'
+import { RouteSnapshot } from '@/app/stores/routeSnapshot'
 
 // 是否已登录跳转控制
 export const authGuard = (route: ActivatedRouteSnapshot) => {
   const router = inject(Router)
+  const routeSnapshot = inject(RouteSnapshot)
+  // 更新 routeSnapshot store
+  routeSnapshot.setData(route)
   return isAuthenticated.value.pipe(
     map((val) => {
       if (val) {
         if (route.data['needAuth'] === false) {
-          router.navigate([''])
+          router.navigate([''], { replaceUrl: true })
           return false
         }
       } else {
         if (route.data['needAuth'] == true) {
-          router.navigate(['login'])
+          router.navigate(['login'], { replaceUrl: true })
           return false
         }
       }
