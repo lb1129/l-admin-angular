ec34e8d72faf2f9525a41d3ba80066a6ad9eb171
leiboT <superviho1@gmail.com>
2023-07-04 00:04:41 +0800

    chore: 涉及异步 menuStore data 改为 Subject

--- src/app/stores/menu.ts
+++ src/app/stores/menu.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core'
 import type { MenuDataItemType } from '@/app/pages/personal-center/types'
+import { ReplaySubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuStore {
-  data: MenuDataItemType[] = []
+  data = new ReplaySubject<MenuDataItemType[]>(1)
 
   setData(newData: MenuDataItemType[]) {
-    this.data = newData
+    this.data.next(newData)
   }
 }
