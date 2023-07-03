3b8d32708bfd97cc2ce983527deb525ce890f4d2
leiboT <superviho1@gmail.com>
2023-07-03 20:41:53 +0800

    chore: 已登录获取菜单及用户信息并更新store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+import { AuthenticateService } from '@/app/pages/authenticate/services'
+import { PersonalCenterService } from '@/app/pages/personal-center/service'
+
+import { MenuStore } from '@/app/stores/menu'
+import { UserInfoStore } from '@/app/stores/userInfo'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private authenticateService: AuthenticateService,
+    private personalCenterService: PersonalCenterService,
+    private menuStore: MenuStore,
+    private userInfoStore: UserInfoStore
+  ) {}
+  ngOnInit() {
+    this.authenticateService.isLogin().subscribe({
+      next: () => {
+        // 获取菜单
+        this.personalCenterService.getMenu().subscribe((menuRes) => {
+          // 更新 menuStore store
+          this.menuStore.setData(menuRes.data)
+          // 获取用户信息
+          this.personalCenterService.getUserInfo().subscribe((userInfoRes) => {
+            // 更新 userInfo store
+            this.userInfoStore.setData(userInfoRes.data)
+          })
+        })
+      }
+    })
+  }
+}
