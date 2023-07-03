128a7a2c5ac84ae898d5a3368d01e99f0274c7b3
leiboT <superviho1@gmail.com>
2023-07-04 00:01:02 +0800

    chore: 名称末尾加上store

--- src/app/stores/routeSnapshot.ts
+++ src/app/stores/routeSnapshot.ts
@@ -4,7 +4,7 @@ import { ActivatedRouteSnapshot } from '@angular/router'
 @Injectable({
   providedIn: 'root'
 })
-export class RouteSnapshot {
+export class RouteSnapshotStore {
   data!: ActivatedRouteSnapshot
 
   setData(newData: ActivatedRouteSnapshot) {
