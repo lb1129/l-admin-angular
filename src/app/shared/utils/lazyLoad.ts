e4748dbab34a3036b4c2cf18e369d49e27237490
leiboT <superviho1@gmail.com>
2023-07-04 18:21:55 +0800

    chore: lazyLoad工具函数

---
+++ src/app/shared/utils/lazyLoad.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { AppHostDirective } from './host.directive'
+import { PageLoadingComponent } from '../page-loading/page-loading.component'
+
+export const lazyLoad = (moduleName: string, componentName: string) => {
+  // TODO fix ID 重复
+  @Component({
+    imports: [AppHostDirective, PageLoadingComponent],
+    selector: 'app-lazy-load',
+    template: `<ng-template appHost />`,
+    standalone: true
+  })
+  class LazyLoadComponent implements OnInit {
+    @ViewChild(AppHostDirective, { static: true }) appHost!: AppHostDirective
+
+    ngOnInit(): void {
+      this.appHost.viewContainerRef.createComponent(PageLoadingComponent)
+      import(`../../pages/${moduleName}/${componentName}/${componentName}.component`).then(
+        (res) => {
+          this.appHost.viewContainerRef.clear()
+          this.appHost.viewContainerRef.createComponent(res.default)
+        }
+      )
+    }
+  }
+  return LazyLoadComponent
+}
