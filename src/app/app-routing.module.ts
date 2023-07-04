79367918b9fe91ae7c0597843b94bd3e40be464f
leiboT <superviho1@gmail.com>
2023-07-04 18:23:57 +0800

    chore: 动态路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,87 +3,67 @@ import { RouterModule, Routes } from '@angular/router'
 import { environment } from '@/environments/environment'
 import { authGuard } from '@/app/auth/guard'
 
-const routes: Routes = [
+import { PageLoadingComponent } from '@/app/shared/page-loading/page-loading.component'
+import { lazyLoad } from '@/app/shared/utils/lazyLoad'
+
+const baseRoutes: Routes = [
   {
     path: '',
     canActivate: [authGuard],
     canActivateChild: [authGuard],
-    loadComponent: () => import('./pages/index/index/index.component'),
-    data: { needAuth: true },
+    component: lazyLoad('index', 'index'),
+    // loadComponent: () => import('./pages/index/index/index.component'),
+    data: { needAuth: true, menuName: 'index' },
     children: [
       {
         path: '',
-        loadComponent: () => import('./pages/index/home/home.component'),
-        data: { needAuth: true }
-      },
-      {
-        path: 'productManagement',
-        children: [
-          { path: '', redirectTo: 'productList', pathMatch: 'full' },
-          {
-            path: 'productList',
-            loadComponent: () =>
-              import('./pages/product-management/product-list/product-list.component'),
-            data: { needAuth: true }
-          },
-          {
-            path: 'productDetail/:id',
-            loadComponent: () =>
-              import('./pages/product-management/product-detail/product-detail.component'),
-            data: { needAuth: true }
-          },
-          {
-            path: 'productAddOrEdit',
-            redirectTo: 'productAddOrEdit/'
-          },
-          {
-            path: 'productAddOrEdit/:id',
-            loadComponent: () =>
-              import(
-                './pages/product-management/product-add-or-edit/product-add-or-edit.component'
-              ),
-            data: { needAuth: true }
-          }
-        ]
+        component: lazyLoad('index', 'home'),
+        // loadComponent: () => import('./pages/index/home/home.component'),
+        data: { needAuth: true, menuName: 'homePage' }
       },
       {
         path: 'personalCenter',
-        loadComponent: () => import('./pages/personal-center/index/index.component'),
-        data: { needAuth: true }
+        component: lazyLoad('personal-center', 'index'),
+        // loadComponent: () => import('./pages/personal-center/index/index.component'),
+        data: { needAuth: true, menuName: 'personalCenter' }
       }
     ]
   },
   {
     path: 'login',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/authenticate/login/login.component'),
+    component: lazyLoad('authenticate', 'login'),
+    // loadComponent: () => import('./pages/authenticate/login/login.component'),
     data: { needAuth: false }
   },
   {
     path: 'register',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/authenticate/register/register.component'),
+    component: lazyLoad('authenticate', 'register'),
+    // loadComponent: () => import('./pages/authenticate/register/register.component'),
     data: { needAuth: false }
   },
   {
     path: 'findPassword',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/authenticate/find-password/find-password.component'),
+    component: lazyLoad('authenticate', 'find-password'),
+    // loadComponent: () => import('./pages/authenticate/find-password/find-password.component'),
     data: { needAuth: false }
   },
   {
     path: 'privacyPolicy',
-    loadComponent: () => import('./pages/sundry/privacy-policy/privacy-policy.component')
+    component: lazyLoad('sundry', 'privacy-policy')
+    // loadComponent: () => import('./pages/sundry/privacy-policy/privacy-policy.component')
   },
   {
     path: '**',
-    loadComponent: () => import('./pages/sundry/not-found/not-found.component')
+    component: PageLoadingComponent
   }
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
+    RouterModule.forRoot(baseRoutes, {
       useHash: environment.NOT_SUPPORT_HISTORY
     })
   ],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core'
+import { Route, Data, Router } from '@angular/router'
 
 import { AuthenticateService } from '@/app/pages/authenticate/services'
 import { PersonalCenterService } from '@/app/pages/personal-center/service'
 
 import { MenuStore } from '@/app/stores/menu'
 import { UserInfoStore } from '@/app/stores/userInfo'
+import type { MenuDataItemType } from '@/app/pages/personal-center/types'
+import { lazyLoad } from '@/app/shared/utils/lazyLoad'
 
 @Component({
   selector: 'app-root',
@@ -16,9 +19,19 @@ export class AppComponent implements OnInit {
     private authenticateService: AuthenticateService,
     private personalCenterService: PersonalCenterService,
     private menuStore: MenuStore,
-    private userInfoStore: UserInfoStore
+    private userInfoStore: UserInfoStore,
+    private router: Router
   ) {}
+
+  getChildrenPath(path: string) {
+    if (/^\/.*/.test(path)) {
+      return path.slice(1)
+    }
+    return path
+  }
+
   ngOnInit() {
+    // 已登录 初始菜单数据 用户信息
     this.authenticateService.isLogin().subscribe({
       next: () => {
         // 获取菜单
@@ -31,7 +44,84 @@ export class AppComponent implements OnInit {
             this.userInfoStore.setData(userInfoRes.data)
           })
         })
+      },
+      error: () => {
+        // 未登录 将 menu store 置为空
+        this.menuStore.setData([])
       }
+    })
+    // 用户菜单数据生成动态路由
+    this.menuStore.data.subscribe((menuData) => {
+      const generateRoutes = (list: MenuDataItemType[], indexRoute?: Route) => {
+        const res: Route[] = []
+        if (indexRoute) {
+          res.push(indexRoute)
+        }
+        list.forEach((record) => {
+          const children = record.children
+          let path = this.getChildrenPath(record.path)
+          if (children && children.length) {
+            const redirectChild = children.find((child) => !child.hidden)
+            let redirectRoute: Route | undefined
+            if (redirectChild && redirectChild.pageUrl) {
+              redirectRoute = {
+                path: '',
+                redirectTo: this.getChildrenPath(redirectChild.path),
+                pathMatch: 'full'
+              }
+            }
+            res.push({
+              path,
+              children: generateRoutes(children, redirectRoute)
+            })
+          } else if (record.pageUrl) {
+            const data: Data = { needAuth: true, menuName: record.name }
+            if (record.hidden) data['hidden'] = true
+            const [moduleName, componentName] = record.pageUrl.split('/')
+            const formatComponentName = componentName
+              .replace(/\B([A-Z])/g, '-$1')
+              .toLocaleLowerCase()
+
+            // angular路由不支持可选参数 例如：productAddOrEdit/:id?
+            // NOTE 如果不需要兼容 vue react 可以在菜单管理配成两条菜单路由
+            const haveOptionalParameters = /:.+\?$/.test(record.path)
+            if (haveOptionalParameters) {
+              res.push({
+                path: path.replace(/\/:.+$/, ''),
+                redirectTo: path.replace(/:.+$/, '')
+              })
+              path = path.replace('?', '')
+            }
+            res.push({
+              path,
+              component: lazyLoad(moduleName, formatComponentName),
+              // loadComponent: () =>
+              //   import(
+              //     `./pages/${moduleName}/${formatComponentName}/${formatComponentName}.component`
+              //   ),
+              data
+            })
+          }
+        })
+        return res
+      }
+      const routes = generateRoutes(menuData)
+
+      const baseRoutes = this.router.config
+      // 将动态路由插入
+      const insertTo = baseRoutes[0].children
+      if (insertTo && insertTo.length) {
+        baseRoutes[0].children = [insertTo[0], ...routes, insertTo[insertTo.length - 1]]
+      }
+      // 删除404转圈
+      delete baseRoutes[baseRoutes.length - 1].component
+      // 为404路由添加loadComponent
+      baseRoutes[baseRoutes.length - 1].loadComponent = () =>
+        import('./pages/sundry/not-found/not-found.component')
+      // 重设路由
+      this.router.resetConfig([...baseRoutes])
+      // 触发重新匹配
+      this.router.navigate([], { replaceUrl: true })
     })
   }
 }
