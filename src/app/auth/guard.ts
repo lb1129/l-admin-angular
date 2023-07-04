cbb9159b823276584a2694dc59d1f0f91af4090c
leiboT <superviho1@gmail.com>
2023-07-04 18:16:16 +0800

    chore: 删除RouteSnapshotStore

--- src/app/auth/guard.ts
+++ src/app/auth/guard.ts
@@ -2,14 +2,10 @@ import { inject } from '@angular/core'
 import { ActivatedRouteSnapshot, Router } from '@angular/router'
 import isAuthenticated from '@/app/auth/isAuthenticated'
 import { map } from 'rxjs'
-import { RouteSnapshotStore } from '@/app/stores/routeSnapshot'
 
 // 是否已登录跳转控制
 export const authGuard = (route: ActivatedRouteSnapshot) => {
   const router = inject(Router)
-  const routeSnapshotStore = inject(RouteSnapshotStore)
-  // 更新 routeSnapshot store
-  routeSnapshotStore.setData(route)
   return isAuthenticated.value.pipe(
     map((val) => {
       if (val) {
