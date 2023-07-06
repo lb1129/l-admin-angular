89ebd9861e869c042ceb50f48dfc15c3b044f91c
leiboT <superviho1@gmail.com>
2023-07-06 22:11:09 +0800

    chore: 改为Observable流获取权限信息

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core'
 import { MenuStore } from '@/app/stores/menu'
 import type { OperateAuthType, MenuDataItemType } from '@/app/pages/personal-center/types'
 import { GET_ACTIVE_ROUTE, GET_ACTIVE_ROUTE_TYPE } from '@/app/shared/utils/getActiveRoute'
+import { Observable, of, switchMap } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,6 @@ export class Auth {
     @Inject(GET_ACTIVE_ROUTE) private getActiveRoute: GET_ACTIVE_ROUTE_TYPE
   ) {}
 
-  operateAuth: OperateAuthType = {}
-  // TODO 其他权限...
-
   operateAuthValueToDisabled(operateAuthValue?: number) {
     // 值为0 无权限 返回true
     // 值为空 或 1 有权限 则返回false
@@ -22,21 +20,23 @@ export class Auth {
     return false
   }
 
-  init() {
-    this.menuStore.data.subscribe((menuData) => {
-      let operateAuth: OperateAuthType = {}
-      let iterativeMenuData = [...menuData]
-      while (iterativeMenuData.length) {
-        const record = iterativeMenuData.shift() as MenuDataItemType
+  getOperateAuth(operate: keyof OperateAuthType): Observable<boolean> {
+    return this.menuStore.data.pipe(
+      switchMap((menuData) => {
+        let operateAuth: OperateAuthType = {}
+        let iterativeMenuData = [...menuData]
         const { route } = this.getActiveRoute()
-        if (record.path === `/${route.routeConfig?.path}`) {
-          if (record.operateAuth) operateAuth = record.operateAuth
-          break
-        } else if (record.children && record.children.length) {
-          iterativeMenuData = [...record.children, ...record.children]
+        while (iterativeMenuData.length) {
+          const record = iterativeMenuData.shift() as MenuDataItemType
+          if (record.path === `/${route.routeConfig?.path}`) {
+            if (record.operateAuth) operateAuth = record.operateAuth
+            break
+          } else if (record.children && record.children.length) {
+            iterativeMenuData = [...record.children, ...record.children]
+          }
         }
-      }
-      this.operateAuth = operateAuth
-    })
+        return of(this.operateAuthValueToDisabled(operateAuth[operate]))
+      })
+    )
   }
 }
