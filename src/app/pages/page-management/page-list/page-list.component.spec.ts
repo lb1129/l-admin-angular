c9f49e2cde1035d9833d5d1b91c7de152cf308b0
leiboT <superviho1@gmail.com>
2023-07-07 00:30:00 +0800

    chore: 页面管理

---
+++ src/app/pages/page-management/page-list/page-list.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { PageListComponent } from './page-list.component'
+
+describe('PageListComponent', () => {
+  let component: PageListComponent
+  let fixture: ComponentFixture<PageListComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    fixture = TestBed.createComponent(PageListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ src/app/pages/page-management/page-list/page-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-page-list',
+  templateUrl: './page-list.component.html',
+  styleUrls: ['./page-list.component.less'],
+  standalone: true
+})
+export default class PageListComponent {}
