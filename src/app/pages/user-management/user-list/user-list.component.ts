640e0eeca9a7508b989009b4b98a896b731841e7
leiboT <superviho1@gmail.com>
2023-07-07 00:30:20 +0800

    chore: 用户管理

---
+++ src/app/pages/user-management/user-list/user-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.less'],
+  standalone: true
+})
+export default class UserListComponent {}
