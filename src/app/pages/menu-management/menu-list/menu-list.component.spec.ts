d93eff79a36ff91e34533e21aed955ab0ecb1f5f
leiboT <superviho1@gmail.com>
2023-07-07 00:29:41 +0800

    chore: 菜单管理

---
+++ src/app/pages/menu-management/menu-list/menu-list.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { MenuListComponent } from './menu-list.component'
+
+describe('MenuListComponent', () => {
+  let component: MenuListComponent
+  let fixture: ComponentFixture<MenuListComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    fixture = TestBed.createComponent(MenuListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ src/app/pages/menu-management/menu-list/menu-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-menu-list',
+  templateUrl: './menu-list.component.html',
+  styleUrls: ['./menu-list.component.less'],
+  standalone: true
+})
+export default class MenuListComponent {}
