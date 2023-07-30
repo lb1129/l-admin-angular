f6bbcd38d310851ba976ac86fe6852a8a7501b06
leiboT <superviho1@gmail.com>
2023-07-31 02:07:17 +0800

    fix: infinite loop

--- src/app/utils/custom-reuse-strategy.ts
+++ src/app/utils/custom-reuse-strategy.ts
@@ -1,7 +1,12 @@
-import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router'
+import {
+  ActivatedRouteSnapshot,
+  DetachedRouteHandle,
+  Route,
+  RouteReuseStrategy
+} from '@angular/router'
 
 export class CustomReuseStrategy implements RouteReuseStrategy {
-  private cacheRouters = new Map<string, DetachedRouteHandle>()
+  private handlers: Map<Route, DetachedRouteHandle> = new Map()
 
   needDetach = false
 
@@ -35,33 +40,20 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 
   // 存储分离的路由
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    if (!route.routeConfig) return
     // 仅缓存一个
-    this.cacheRouters.clear()
-    this.cacheRouters.set(this.getFullRouteURL(route), handle)
+    this.handlers.clear()
+    this.handlers.set(route.routeConfig, handle)
   }
 
   // 确定是否应重新连接此路由（及其子树）
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return !!this.cacheRouters.get(this.getFullRouteURL(route))
+    return !!route.routeConfig && !!this.handlers.get(route.routeConfig)
   }
 
   // 检索以前存储的路由
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    const cache = this.cacheRouters.get(this.getFullRouteURL(route))
-    return cache || null
-  }
-
-  //获取完整路由路径
-  private getFullRouteURL(route: ActivatedRouteSnapshot): string {
-    const { pathFromRoot } = route
-    let fullRouteUrlPath: string[] = []
-    pathFromRoot.forEach((item: ActivatedRouteSnapshot) => {
-      fullRouteUrlPath = fullRouteUrlPath.concat(this.getRouteUrlPath(item))
-    })
-    return `/${fullRouteUrlPath.join('/')}`
-  }
-
-  private getRouteUrlPath(route: ActivatedRouteSnapshot) {
-    return route.url.map((urlSegment) => urlSegment.path)
+    if (!route.routeConfig || !this.handlers.has(route.routeConfig)) return null
+    return this.handlers.get(route.routeConfig) as DetachedRouteHandle
   }
 }
